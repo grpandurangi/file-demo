78fa4889e889198f2851aa0cf504cb45de547f67
Jenkins <jenkins@jenkins.c.csd-automation.internal>
2017-06-05 11:28:58 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -20,8 +20,8 @@ if [[ "$?" -eq "0" ]]; then
 
 else
 
-#/usr/lib/google-cloud-sdk/bin/kubectl apply -f service.yml
-/usr/lib/google-cloud-sdk/bin/kubectl expose deployment my-file-demo --type=LoadBalancer --name=my-file-demo-service ;
+/usr/lib/google-cloud-sdk/bin/kubectl apply -f service.yml
+#/usr/lib/google-cloud-sdk/bin/kubectl expose deployment my-file-demo --type=LoadBalancer --name=my-file-demo-service ;
 
 #--load-balancer-ip="104.197.129.72"
 
