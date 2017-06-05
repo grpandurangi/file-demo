99781cc191f544b248d5bf2e572f1b02a65f2900
Jenkins <jenkins@jenkins.c.csd-automation.internal>
2017-06-05 09:16:18 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 
 gcloud config set compute/zone us-east1-d
 gcloud config set project csd-automation
+
+gcloud container clusters describe file-demo
+
+if [[ "$?" -eq "0" ]]; then
+
+
 /usr/lib/google-cloud-sdk/bin/kubectl apply -f deployment.yml
 /usr/lib/google-cloud-sdk/bin/kubectl get service my-file-demo-service
 
@@ -17,4 +23,12 @@ else
 /usr/lib/google-cloud-sdk/bin/kubectl expose deployment my-file-demo --type=LoadBalancer --name=my-file-demo-service
 
 fi
+
+else
+
+echo "Google Container Engine is not created";
+exit 1;
+
+fi
+
 
