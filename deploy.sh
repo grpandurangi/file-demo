9e273b16d21aeb83bcbe3104f4293477f7eb3b26
Jenkins <jenkins@jenkins.c.csd-automation.internal>
2017-06-05 08:54:01 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-docker login -e grpandurangi@gmail.com -u _json_key -p "$(cat /tmp/key.json)" https://gcr.io
-docker build -t gcr.io/csd-automation/filedemo/file-demo:latest .
-docker push gcr.io/csd-automation/filedemo/file-demo:latest
 gcloud config set compute/zone us-east1-d
 gcloud config set project csd-automation
+/usr/lib/google-cloud-sdk/bin/kubectl apply -f deployment.yml
+/usr/lib/google-cloud-sdk/bin/kubectl get service my-file-demo-service
 
-#/usr/lib/google-cloud-sdk/bin/kubectl delete deployments my-file-demo
+if [[ "$?" eq "0" ]]; then
+
 /usr/lib/google-cloud-sdk/bin/kubectl get pods
 /usr/lib/google-cloud-sdk/bin/kubectl set image deployment/my-file-demo my-file-demo=gcr.io/csd-automation/filedemo/file-demo:latest
 /usr/lib/google-cloud-sdk/bin/kubectl set image deployment/my-file-demo my-file-demo=gcr.io/csd-automation/filedemo/file-demo
 /usr/lib/google-cloud-sdk/bin/kubectl get pods
+
+else
+
+/usr/lib/google-cloud-sdk/bin/kubectl expose deployment my-file-demo --type=LoadBalancer --name=my-file-demo-service
+
+fi
+
